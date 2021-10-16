782078dedc1f650c202d448d83aaba50a7f53aab
yan <yan-wyb@foxmail.com>
2021-10-16 17:07:53 +0800

    update
    Signed-off-by: yan <yan-wyb@foxmail.com>

---
+++ darknet/yolov4/README.md
@@ -0,0 +1,1 @@
+https://gitlab.com/yan518/models-zoo-big/-/blob/master/yolov4-leaky-416.weights
--- darknet/yolov4/download_weights.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget https://gitlab.com/yan518/models-zoo-big/-/raw/master/yolov4-leaky-416.weights?inline=false
